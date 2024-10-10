7fcd195f684b39198c72ec7e0e2dbe0d21e682f3
w-foster <william.jfoster@yahoo.com>
2024-10-11 00:32:20 +0100

    Cleaned up BaseProcessor header file.

--- include/BaseProcessor.h
+++ include/BaseProcessor.h
@@ -4,34 +4,16 @@
 #include <unordered_map>
 #include <utility>
 
+/*
 struct BaseInfo {
     int gold, elixir, dark;
     int trophies_win, trophies_lose;
 
     BaseInfo() {}
 };
+*/
 
 class BaseProcessor {
-
-    /*
-    HBITMAP current_bmp
-    map regions = {
-        {"GOLD AVAILABLE": {x1, x2, y1, y2}}
-    }
-    BaseInfo base_info;
-
-    updateCurrentBitmap()
-    cropBitmap(region name string e.g. "GOLD AVAILABLE")
-    preprocessBitmap(bmp, grayscale / high contrast) --> pass e.g. the cropped bitmap
-        - This may do, grayscale, or contrast up#
-    findText(bmp) --> find the text e.g. 875,125 (amount of Gold)
-
-    higher level methods?
-    cropLoot(bmp) -> this might make 3 images, one for gold, elixir, DE each
-
-    NON-IMAGE METHODS:
-    checkBaseSuitability(base_info) --> check according to some params
-    */
 
     cv::Mat curr_bmp; // stores current bitmap as an OpenCV Matrix
     const std::unordered_map<std::string, std::vector<double>> regions;
@@ -47,10 +29,13 @@ public:
 
     bool updateCurrentBitmap(const std::string &new_bmp_path);
     bool preprocessBitmap(std::string transformation_type);
+
     void setSuitableMetricRanges(int min_gold, int min_elixir=0, int min_dark=0, int min_trophies_win=0, int max_trophies_lose=59);
+
     bool readRegion(std::string region_name);
     bool readAllMetrics();
     int processMetricText(std::string region_name, std::string &text);
+
     bool isBaseSuitable();
 
 };
